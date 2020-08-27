029fdf221cf5a19795c8d48037b5cfc02e28b436
Andrew Raines <rainesand26@gmail.com>
2020-08-27 18:49:03 -0400

    working on feed page

---
+++ public/js/feed.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    
+    
+    
+    // function that checks if you have any friends or movies already added to your feed
+    // if/else
+
+    
+    // function that updates feed actively via button or swipe or refresh
+
+
+
+
+    // function that updates the feed passively (refresh rate like 30s-1min)
+
+})
